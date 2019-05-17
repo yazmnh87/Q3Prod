eb5bc970d9ec702fec643610fb37f9cdb09f57ca
William Violette <violettewj@gmail.com>
2019-05-17 11:46:37 -0600

    additional changes usermgmt page

--- Q3APP2/client/src/components/pages/Usermgmt.js
+++ Q3APP2/client/src/components/pages/Usermgmt.js
@@ -29,7 +29,7 @@ class Usermgmt extends Component {
   render(props) {
     const users = this.props.allUsers.map(user => {
         return (<>
-                    <tr className="">
+                    <tr className="mt-2">
                         <td className="pr-5">{user.firstName + " " + user.lastName}</td>
                         <td className="pr-5">{user.email}</td>
                         <td className="pr-5">{user.date.slice(0,10)}</td>
@@ -47,9 +47,10 @@ class Usermgmt extends Component {
     return (
       <>
         <Navbar/>
-        <div className="mt-3 ml-5">
+        <div className="usermgmt pt-3">
+        <div className="ml-5">
         <span style={{fontSize: 20}} className="border border-outline-light">Add New User</span>
-        <div>
+        <div className="mt-3">
         <label>First Name</label>
         <input onChange={this.onChange} name="firstName" value={this.state.firstName} className="mr-3"></input>
         <label>Last Name</label>
@@ -60,7 +61,7 @@ class Usermgmt extends Component {
             <option>Admin</option>
             <option>User</option>
         </select>
-        <button onClick={""}>Add User</button>
+        <button onClick={""} className="btn btn-outline-dark">Add User</button>
         </div>
         </div>
         <div className="container mt-4">
@@ -70,17 +71,18 @@ class Usermgmt extends Component {
         <table>
             <thead>
                 <tr>
-                    <th>User</th>
-                    <th>Email</th>
-                    <th>Date Created</th>
-                    <th>Role</th>
-                    <th>Status</th>
+                    <th className="border-bottom border-dark">User</th>
+                    <th className="border-bottom border-dark">Email</th>
+                    <th className="border-bottom border-dark">Date Created</th>
+                    <th className="border-bottom border-dark">Role</th>
+                    <th className="border-bottom border-dark">Status</th>
                 </tr>
             </thead>
             <tbody>
                 {users}
             </tbody>
         </table>
+        </div>
         </div>
       </>
     )
