fd1d8c1357b0c5782c33254415939555e57a7881
William Violette <violettewj@gmail.com>
2019-08-31 19:26:15 -0600

    updated for heroku

--- Q3APP2/client/src/components/pages/Profile.js
+++ Q3APP2/client/src/components/pages/Profile.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
 import Navbar from '../Navbar'
 import { withProvider } from '../../context'
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
 
  class Profile extends Component {
      state={
-         firstName: this.props.currentUserName,
-         lastName: this.props.currentUserLastName,
-         phoneNumber: "",
-         birthday: "",
-         careerInterests: [],
-         hobbies: []
+        firstName: this.props.currentUserName,
+        lastName: this.props.currentUserLastName,
+        phoneNumber: "",
+        birthday: "",
+        careerInterests: [],
+        hobbies: [],
+        startDate: new Date()
      }
 
      onChange = (e) => {
@@ -18,30 +21,44 @@ import { withProvider } from '../../context'
         })
     }
 
+    handleChange = (date) => {
+        this.setState({
+          startDate: date
+        });
+      }
+
     render() {
 
         return (
             <>
             <Navbar />
             <div className="profilebackground">
-            <div className="col sm 8">
+            <div className="col-sm-12">
             <div className="row d-flex justify-content-center align-items-center ml-3 mr-3 pt-5">
             <label>First Name: </label>
             <input className="form-control col sm 4 mr-3" value={this.state.firstName} name="" onChange={this.onChange} disabled/>
             <label>Last Name: </label>
-            <input className="form-control col sm 4 ml-3 mr-3" value={this.state.lastName} name="" onChange={this.onChange} disabled/>
+            <input className="form-control col-sm-4 ml-3 mr-3" value={this.state.lastName} name="" onChange={this.onChange} disabled/>
             <label>DOB: </label>
-            <input className="form-control col sm 4 mr-3 ml-3" value={""} name="" onChange={this.onChange}/>
+            <DatePicker className="form-control"
+            selected={this.state.startDate}
+            onChange={this.handleChange}
+            />
+            {/* <input className="form-control col sm 4 mr-3 ml-3" value={""} name="" onChange={this.onChange}/> */}
             </div>
             </div>
             {/* <div className='col d-flex justify-content-center mt-3'> */}
-            <div className="row ml-5 mt-4 justify-content-center">
-            <label>Email: </label>
-            <input className="form-control w-50" value={this.state.email} name="" onChange={this.onChange}/>
+            <div className="col-sm-12">
+            <div className="row ml-3 mr-3">
+            <div className="d-inline mr-2" style={{width:'49%'}}>
+            <label className="d-inline">Email: </label>
+            <input className="form-control" value={this.state.email} name="" onChange={this.onChange}/>
             </div>
-            <div className="row ml-4 mt-4 justify-content-center">
-            <label>Phone: </label>
-            <input className="form-control w-50" value={""} name="" onChange={this.onChange}/>
+            <div className="d-inline" style={{width:'49%'}}>
+            <label className="d-inline">Phone: </label>
+            <input className="form-control" value={""} name="" onChange={this.onChange}/>
+            </div>
+            </div>
             </div>
             {/* </div> */}
             <div className="row ml-4 mt-4 justify-content-center">
@@ -53,7 +70,7 @@ import { withProvider } from '../../context'
             <input className="form-control w-50" value={""} name="" onChange={this.onChange}/>
             </div>
             <div className="row ml-4 mt-4 justify-content-center">
-                <button>Updated</button>
+                <button>Update</button>
             </div>
             </div>
             </>
--- Q3APP2/server/server.js
+++ Q3APP2/server/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const passport = require('passport')
+const config = require('config')
+const db = config.get('mongoURI')
+const path = require('path')
 // const users = require('./routes/users')
 const app = express();
-const port = 5000
-
+const PORT = process.env.PORT || 5000
 //passport middleware
 app.use(passport.initialize())
 
@@ -16,7 +18,17 @@ app.use('/api/responses', require('./routes/response'))
 app.use('/api/questions', require('./routes/questions'))
 app.use('/api/users', require('./routes/users'))
 app.use('/api/profile', require('./routes/profile'))
-app.listen(port, () => console.log(`server running on ${port}`))
 
 
-mongoose.connect('mongodb://localhost:27017/InTouch', {useNewUrlParser: true}).then(() => console.log('connected to MongoDB'))
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+
+    app.get(*, (req, res) =>{
+        res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html' ))
+    } )
+}
+
+app.listen(PORT, () => console.log(`server running on ${PORT}`))
+
+
+mongoose.connect(db, {useNewUrlParser: true}).then(() => console.log('connected to MongoDB'))
